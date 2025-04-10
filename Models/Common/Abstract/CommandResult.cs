3dadbd25299202b52f3d2b0df0b4653fb4d5d0b9
usr <usr@PC1>
2025-04-10 16:47:42 +0300

    Adds New Command  And File Result Models

---
+++ Models/Common/Abstract/CommandResult.cs
@@ -0,0 +1,10 @@
+﻿using TestApp_Wpf.Models.Common.Enumerations;
+
+namespace TestApp_Wpf.Models.Common.Abstract;
+
+public class CommandResult<T>
+{
+    public T? Value { get; }
+    public CommandStatus Status { get; } = CommandStatus.DEFAULT;
+    public List<Exception> Errors { get; } = [];
+}
---
+++ Models/ParsedModels/ParsedFileResult.cs
@@ -0,0 +1,15 @@
+﻿using MimeKit;
+using System.IO;
+using TestApp_Wpf.Models.DomainModels;
+using TestApp_Wpf.Services.Parsing.Interfaces;
+
+namespace TestApp_Wpf.Models.ParsedModels;
+
+public record ParsedFileResult(
+    ContentType ContentType, 
+    string FileName, 
+    string Path, 
+    long Length) 
+    : IParsingFile;
+
+
