6fbc4a5ccf6e2d126027e1a40be2723e599d5657
usr <usr@PC1>
2025-04-25 15:12:18 +0300

    Reimplements Command Logics

---
+++ Infrastructure/Commands/Abstract/BaseCommand.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TestApp_Wpf.Models.Common.Abstract;
+
+namespace TestApp_Wpf.Infrastructure.Commands.Abstract;
+
+public class BaseCommand : ICommand
+{    
+    private readonly Func<object?, Task>? _asyncExecute;
+    private readonly Action<object?>?     _syncExecute;
+    private readonly Func<object?, bool>? _canExecute;
+    private readonly Action<Exception>?   _errorHandler;
+    private bool _isExecuting;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Occurs when changes the command should execute
+    /// </summary>
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public BaseCommand(
+        Func<object?, Task>? asyncExecute = null,
+        Action<object?>? syncExecute = null,
+        Func<object?, bool>? canExecute = null,
+        Action<Exception>? errorHandler = null)
+    {
+        _asyncExecute = asyncExecute;
+        _syncExecute = syncExecute;
+        _canExecute = canExecute;
+        _errorHandler = errorHandler;
+    }
+
+    /// <summary>
+    /// Determines whether the command can execute in its current state
+    /// </summary>
+    public bool CanExecute(object? parameter = null)
+    {
+        if (_isExecuting) return false;
+        return _canExecute?.Invoke(parameter) ?? true;
+    }
+
+    /// <summary>
+    /// Executes the command
+    /// </summary>
+    public async void Execute(object? parameter = null)
+    {
+        if (!CanExecute(parameter)) return;
+
+        try
+        {
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+
+            if (_asyncExecute != null)
+            {
+                await _asyncExecute(parameter)
+                    .ConfigureAwait(false);
+            }
+            else
+            {
+                _syncExecute?
+                    .Invoke(parameter);
+            }
+        }
+        catch (Exception ex) when (_errorHandler != null)
+        {
+            _errorHandler(ex);
+        }
+        finally
+        {
+            _isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+
+    /// <summary>
+    /// Raises the CanExecuteChanged event to force requery of command state
+    /// </summary>
+    public void RaiseCanExecuteChanged() => CommandManager.InvalidateRequerySuggested();
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null!)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+    }
+}
+
---
+++ Infrastructure/Commands/MainViewModel.Commands/LoadTestObjectsCommand.cs
@@ -0,0 +1,91 @@
+﻿using FluentValidation.Results;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TestApp_Wpf.Infrastructure.Commands.Abstract;
+using TestApp_Wpf.Infrastructure.Extensions;
+using TestApp_Wpf.Models.Common.Abstract;
+using TestApp_Wpf.Models.Common.Enumerations;
+using TestApp_Wpf.Models.DomainModels;
+using TestApp_Wpf.Models.ParsedModels;
+using TestApp_Wpf.Services.FileDialog.Interfaces;
+using TestApp_Wpf.Services.Parsing.Interfaces;
+using TestApp_Wpf.Services.Validation.Interfaces;
+
+namespace TestApp_Wpf.Infrastructure.Commands.MainViewModel.Commands;
+
+public class LoadTestObjectsCommand : BaseCommand
+{
+    private readonly IFileDialogService _dialogService;
+    private readonly IValidationService _validator;
+    private readonly IParsingService    _fileParser;
+    private CommandResult<IEnumerable<TestObject>> _result = new();
+
+    public CommandResult<IEnumerable<TestObject>> Result  => _result;
+
+    public LoadTestObjectsCommand(
+        IValidationService fileValidator,                           
+        IFileDialogService dialogService,
+        IParsingService parsingService) 
+    { 
+        _validator = fileValidator;
+        _dialogService = dialogService;
+        _fileParser = parsingService;
+    }
+
+    public async Task ExecuteAsync()
+    {
+        try
+        {
+            // Get file
+            var file = _dialogService.GetFile()
+                .MapParsedFile<ParsedFileResult>();
+
+            // Validate file
+            ValidationResult fileValidation = await _validator
+                .ValidateAsync<ParsedFileResult>(file);
+
+            if (fileValidation.IsValid)
+            {
+                List<IDomainModel> results = await _fileParser
+                    .ParseFileAsync(file);
+
+                // Parse valid model
+                var templateModel = results.FirstOrDefault();
+                if (templateModel is TestObject)
+                {
+                    var models = await _validator
+                        .ValidateDomainModels<TestObject>(results);
+
+                    InitResult(models, CommandStatus.SUCCESS);
+                    return;
+                }
+                else InitResult(
+                    value: [],
+                    status: CommandStatus.ERROR,
+                    error: results.Count == 0
+                           ? new Exception($"The provided file doesn`t contain models of type {typeof(TestObject).Name}")
+                           : new InvalidCastException($"{templateModel!.GetType().Name} is not a valid {typeof(TestObject).Name} type"));
+            }
+            else InitResult(
+                    value:  [], 
+                    status: CommandStatus.ERROR, 
+                    error:  new Exception(fileValidation.Errors.FirstOrDefault()!.ErrorMessage));
+        }
+        catch (Exception ex) 
+        { 
+            InitResult([], CommandStatus.ERROR, ex);
+        }
+    }
+
+    private void InitResult(
+        IEnumerable<TestObject> value, 
+        CommandStatus status, 
+        Exception? error = null)
+    {
+        _result.Value = value;
+        _result.Error = error;
+        _result.Status = status;
+
+        OnPropertyChanged("Result");
+    }
+}
